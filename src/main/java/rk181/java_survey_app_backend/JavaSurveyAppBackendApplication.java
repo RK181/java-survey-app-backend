a5857261e6f9a2f5207c8f2b86e17b10f7d0c806
Robert Kazaryan <rk30@gcloud.ua.es>
2024-11-14 16:56:45 +0100

    Initial commit

---
+++ src/main/java/rk181/java_survey_app_backend/JavaSurveyAppBackendApplication.java
@@ -0,0 +1,13 @@
+package rk181.java_survey_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSurveyAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSurveyAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rk181/java_survey_app_backend/JavaSurveyAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package rk181.java_survey_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSurveyAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
