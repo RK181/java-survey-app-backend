30a59abe6fa37b68173c619627a115f06e8f28e1
Robert Kazaryan <rk30@gcloud.ua.es>
2024-11-16 00:10:50 +0100

    Update schema to use BIGINT for IDs and modify total_votes to UNSIGNED

--- sql/schema-1.0.0.sql
+++ sql/schema-1.0.0.sql
@@ -2,35 +2,38 @@ DROP SCHEMA IF EXISTS `survey-app-db`;
 CREATE SCHEMA `survey-app-db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 CREATE TABLE `survey-app-db`.`users` (
-    id INT NOT NULL AUTO_INCREMENT,
+    id BIGINT NOT NULL AUTO_INCREMENT,
     nickname VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     salt VARCHAR(255) NOT NULL,
+    
     PRIMARY KEY (id)
 ) engine=InnoDB;
 
 CREATE TABLE `survey-app-db`.`surveys` (
-    id INT NOT NULL AUTO_INCREMENT,
-    user_id INT NOT NULL,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
     title VARCHAR(255) NOT NULL,
     description TEXT,
-    total_votes INT DEFAULT 0,
+    total_votes INT UNSIGNED DEFAULT 0,
+    
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 ) engine=InnoDB;
 
 CREATE TABLE `survey-app-db`.`options` (
-    id INT NOT NULL AUTO_INCREMENT,
-    survey_id INT NOT NULL,
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    survey_id BIGINT NOT NULL,
     name VARCHAR(255) NOT NULL,
+    total_votes INT UNSIGNED DEFAULT 0,
+    
     PRIMARY KEY (id),
     FOREIGN KEY (survey_id) REFERENCES surveys(id) ON DELETE CASCADE
 ) engine=InnoDB;
 
 CREATE TABLE `survey-app-db`.`options_users` (
-    user_id INT NOT NULL,
-    option_id INT NOT NULL,
-    total_votes INT DEFAULT 0,
+    user_id BIGINT NOT NULL,
+    option_id BIGINT NOT NULL,
 
     PRIMARY KEY (user_id, option_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
