f763d5989f040defaea4d328185082affa10da1f
Robert Kazaryan <rk30@gcloud.ua.es>
2024-11-24 11:58:45 +0100

    Add methods to find and check existence of survey options by survey ID and option ID

--- src/main/java/rk181/java_survey_app_backend/survey_options/SurveyOptionRepository.java
+++ src/main/java/rk181/java_survey_app_backend/survey_options/SurveyOptionRepository.java
@@ -1,9 +1,12 @@
 package rk181.java_survey_app_backend.survey_options;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SurveyOptionRepository extends JpaRepository<SurveyOption, Long> {
-
+    Optional<SurveyOption> findBySurveyIdAndId(Long surveyId, Long id);
+    boolean existsBySurveyIdAndId(Long surveyId, Long id);
 }
