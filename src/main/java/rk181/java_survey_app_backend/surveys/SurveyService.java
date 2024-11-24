945edabe14eaef959b169973450dbc68e1cdf34b
Robert Kazaryan <rk30@gcloud.ua.es>
2024-11-24 12:21:01 +0100

    Refactor SurveyService to improve transaction management and enhance method documentation

--- src/main/java/rk181/java_survey_app_backend/surveys/SurveyService.java
+++ src/main/java/rk181/java_survey_app_backend/surveys/SurveyService.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import rk181.java_survey_app_backend.auth.Auth;
 import rk181.java_survey_app_backend.surveys.dto.SurveyDTO;
 import rk181.java_survey_app_backend.users.User;
@@ -18,17 +18,19 @@ public class SurveyService {
     private final SurveyRepository surveyRepository;
     private final UserRepository userRepository;
 
-
+    // Can be replaced with @RequiredArgsConstructor of Lombok
     public SurveyService(SurveyRepository surveyRepository, UserRepository userRepository) {
         this.surveyRepository = surveyRepository;
         this.userRepository = userRepository;
     }
 
+    /**
+     * Create a survey
+     * @param surveyDTO
+     * @return SurveyDTO with user(owner)
+     */
     public SurveyDTO createSurvey(SurveyDTO surveyDTO) {
-        User authUser = userRepository.findById(Auth.getUserIDFromContext()).orElse(null);
-        if (authUser == null) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not found");
-        }
+        User authUser = userRepository.getReferenceById(Auth.getUserIDFromContext());
         
         Survey survey = Survey.fromDTO(authUser, surveyDTO);
         surveyRepository.save(survey);
@@ -36,6 +38,13 @@ public class SurveyService {
         return new SurveyDTO(survey, survey.getUser());
     }
 
+    /**
+     * Get a survey by id
+     * @param surveyId
+     * @throws ResponseStatusException NOT_FOUND if survey not found
+     * @return SurveyDTO with user(owner) and survey options
+     */
+    @Transactional(readOnly = true)
     public SurveyDTO getSurveyById(Long surveyId) {
         Survey survey = surveyRepository.findById(surveyId).orElse(null);
         if (survey == null) {
@@ -45,21 +54,29 @@ public class SurveyService {
         return new SurveyDTO(survey, survey.getSurveyOptions(), survey.getUser());    
     }
 
+    /**
+     * Get all surveys
+     * @return List of SurveyDTO with user(owner)
+     */
+    @Transactional(readOnly = true)
     public List<SurveyDTO> getAllSurveys() {
         List<Survey> surveys = surveyRepository.findAll();
-
         return SurveyDTO.listFromSurveys(surveys);
     }
 
+    /**
+     * Update a survey by id
+     * @param surveyId
+     * @param surveyDTO
+     * @throws ResponseStatusException NOT_FOUND if survey not found or user is not the owner
+     * @return SurveyDTO with updated fields and user
+     */
     @Transactional
     public SurveyDTO putSurveyById(Long surveyId, SurveyDTO surveyDTO) {
         Long authUserID = Auth.getUserIDFromContext();
         Survey survey = surveyRepository.findById(surveyId).orElse(null);
-        if (survey == null) {
+        if (survey == null || !survey.getUser().getId().equals(authUserID)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Survey not found");
-        }
-        if (!survey.getUser().getId().equals(authUserID)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User is not the owner");
         }
 
         survey.setTitle(surveyDTO.getTitle());
@@ -69,19 +86,16 @@ public class SurveyService {
         return new SurveyDTO(survey, survey.getUser());
     }
 
+    /**
+     * Delete a survey by id
+     * @param surveyId
+     * @throws ResponseStatusException NOT_FOUND if survey not found or user is not the owner
+     */
     public void deleteSurveyById(Long surveyId) {
         Long authUserID = Auth.getUserIDFromContext();
-        Survey survey = surveyRepository.findById(surveyId).orElse(null);
-        if (survey == null) {
+        if (!surveyRepository.existsByUserIdAndId(authUserID, surveyId)) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Survey not found");
         }
-        if (!survey.getUser().getId().equals(authUserID)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "User is not the owner");
-        }
-        surveyRepository.deleteById(surveyId);
-    }
-
-    public void deleteSurvey(Long surveyId) {
         surveyRepository.deleteById(surveyId);
     }
 }
