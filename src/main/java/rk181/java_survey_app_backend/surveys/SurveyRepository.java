932adb834cd2deeef950ea1e8904c3e9142dee74
Robert Kazaryan <rk30@gcloud.ua.es>
2024-11-24 11:58:32 +0100

    Add method to check existence of survey by user ID and survey ID

--- src/main/java/rk181/java_survey_app_backend/surveys/SurveyRepository.java
+++ src/main/java/rk181/java_survey_app_backend/surveys/SurveyRepository.java
@@ -5,5 +5,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SurveyRepository extends JpaRepository<Survey, Long> {
+    boolean existsByUserIdAndId(Long userId, Long id);
 
 }
