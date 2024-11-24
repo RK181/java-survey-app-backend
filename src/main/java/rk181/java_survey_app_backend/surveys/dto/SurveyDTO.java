efdeb8c9fbdcf9b28014621cd087de721f115bb6
Robert Kazaryan <rk30@gcloud.ua.es>
2024-11-24 12:32:26 +0100

    Enhance SurveyDTO with documentation for clarity

--- src/main/java/rk181/java_survey_app_backend/surveys/dto/SurveyDTO.java
+++ src/main/java/rk181/java_survey_app_backend/surveys/dto/SurveyDTO.java
@@ -20,13 +20,27 @@ public class SurveyDTO {
     private Long id;
     private String title;
     private String description;
+    /**
+     * Total votes of the survey, calculated field
+     */
     private Integer total_votes;
+    /**
+     * User who created the survey, ignored in JSON if null
+     */
     private UserDTO user = null;
+    /**
+     * Survey options, ignored in JSON if null
+     */
     private List<SurveyOptionDTO> surveyOptions = null;
     
 
     public SurveyDTO() {}
 
+    /**
+     * Create a SurveyDTO from a Survey
+     * @param survey
+     * @return SurveyDTO without user(owner) and survey options. JSON will not include user(owner) and survey options
+     */
     public SurveyDTO(Survey survey) {
         this.id = survey.getId();
         this.title = survey.getTitle();
@@ -34,6 +48,12 @@ public class SurveyDTO {
         this.total_votes = survey.getTotal_votes();
     }
 
+    /**
+     * Create a SurveyDTO from a Survey and a User
+     * @param survey
+     * @param user
+     * @return SurveyDTO with user(owner). JSON will not include survey options
+     */
     public SurveyDTO(Survey survey, User user) {
         this.id = survey.getId();
         this.title = survey.getTitle();
@@ -43,6 +63,13 @@ public class SurveyDTO {
             this.user = new UserDTO(user.getNickname(), null);
     }
 
+    /**
+     * Create a SurveyDTO from a Survey and a list of SurveyOptions
+     * @param survey
+     * @param surveyOptions
+     * @param user
+     * @return SurveyDTO with user(owner) and survey options
+     */
     public SurveyDTO(Survey survey, List<SurveyOption> surveyOptions, User user) {
         this.id = survey.getId();
         this.title = survey.getTitle();
@@ -54,6 +81,11 @@ public class SurveyDTO {
             this.user = new UserDTO(user.getNickname(), null);
     }
 
+    /**
+     * Convert a list of surveys to a list of SurveyDTO
+     * @param surveys
+     * @return List of SurveyDTO with user(owner)
+     */
     public static List<SurveyDTO> listFromSurveys(List<Survey> surveys) {
         return surveys.stream().map(survey -> new SurveyDTO(survey, survey.getUser())).toList();
     }
