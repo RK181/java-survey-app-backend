5437ce92a955ecf17482ff2ebd90dc71ef5d69a5
Robert Kazaryan <rk30@gcloud.ua.es>
2024-11-25 21:19:10 +0100

    Replace @Min annotation with @Size for password validation in AuthDTO

--- src/main/java/rk181/java_survey_app_backend/auth/dto/AuthDTO.java
+++ src/main/java/rk181/java_survey_app_backend/auth/dto/AuthDTO.java
@@ -1,6 +1,5 @@
 package rk181.java_survey_app_backend.auth.dto;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -12,7 +11,7 @@ public class AuthDTO {
     @Size(min = 5, max = 20, message = "Nickname must be between 5 and 20 characters")
 	private String nickname;
     @NotBlank
-    @Min(value = 12, message = "Password must be at least 12 characters")
+    @Size(min = 12, message = "Password must be at least 12 characters")
     @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).*$", message = "Password must contain at least one lowercase letter, one uppercase letter and one digit")
 	private String password;
 
