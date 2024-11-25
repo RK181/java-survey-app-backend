42a2d45168681641c3aa8f5906067bab8efa6efc
Robert Kazaryan <rk30@gcloud.ua.es>
2024-11-25 18:35:05 +0100

    Enhance UserController and UserService to support pagination for user surveys

--- src/main/java/rk181/java_survey_app_backend/users/UserController.java
+++ src/main/java/rk181/java_survey_app_backend/users/UserController.java
@@ -2,9 +2,11 @@ package rk181.java_survey_app_backend.users;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.websocket.server.PathParam;
 import rk181.java_survey_app_backend.users.dto.UserDTO;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -32,14 +34,19 @@ public class UserController {
 
     @GetMapping("/{nickname}/surveys")
     @ResponseStatus(HttpStatus.OK)
-    public UserDTO getUserAndSurveys(@PathVariable String nickname) {
-        return userService.getUserAndSurveys(nickname);
+    public UserDTO getUserAndSurveys(@PathVariable String nickname, 
+                                     @RequestParam(required = false) Integer offset, 
+                                     @RequestParam(required = false) Integer limit) 
+    {
+        return userService.getUserAndSurveys(nickname, offset, limit);
     }
 
     @GetMapping("/surveys")
     @ResponseStatus(HttpStatus.OK)
-    public UserDTO getUserAndSurveys() {
-        return userService.getAuthUserAndSurveys();
+    public UserDTO getUserAndSurveys(@RequestParam(required = false) Integer offset, 
+                                     @RequestParam(required = false) Integer limit) 
+    {
+        return userService.getAuthUserAndSurveys(offset, limit);
     }
 
     @DeleteMapping
--- src/main/java/rk181/java_survey_app_backend/users/UserService.java
+++ src/main/java/rk181/java_survey_app_backend/users/UserService.java
@@ -1,11 +1,14 @@
 package rk181.java_survey_app_backend.users;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import rk181.java_survey_app_backend.auth.Auth;
+import rk181.java_survey_app_backend.surveys.Survey;
 import rk181.java_survey_app_backend.users.dto.UserDTO;
 
 @Service
@@ -48,29 +51,40 @@ public class UserService {
     }
 
     /**
-     * Get the authenticated user and his surveys
-     * @return The user and the surveys array
+     * Get the authenticated user and his surveys. Offset and limit can be provided. Default limit is 100
+     * @return The user and the surveys array with offset and limit. Default limit is 100
      */
     @Transactional(readOnly = true)
-    public UserDTO getAuthUserAndSurveys() {
+    public UserDTO getAuthUserAndSurveys(Integer offset, Integer limit) {
         Long id = Auth.getUserIDFromContext();
         User user = userRepository.getReferenceById(id);
 
-        return new UserDTO(user.getNickname(), user.getSurveys());
+        List<Survey> surveys = user.getSurveys().stream()
+            .skip(offset != null ? offset : 0)
+            .limit(limit != null && limit != 0? limit : 100)
+            .toList();
+
+        return new UserDTO(user.getNickname(), surveys);
     }
 
     /**
      * Get the user and his surveys by nickname
      * @throws ResponseStatusException NOT_FOUND if user not found
-     * @return The user and the surveys array
+     * @return The user and the surveys array with offset and limit. Default limit is 100
      */
     @Transactional(readOnly = true)
-    public UserDTO getUserAndSurveys(String nickname) {
+    public UserDTO getUserAndSurveys(String nickname, Integer offset, Integer limit) {
         User user = userRepository.findByNickname(nickname).orElse(null);
         if (user == null) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
         }
 
-        return new UserDTO(user.getNickname(), user.getSurveys());
+        List<Survey> surveys = user.getSurveys().stream()
+            .skip(offset != null ? offset : 0)
+            .limit(limit != null && limit != 0? limit : 100)
+            .toList();
+        
+
+        return new UserDTO(user.getNickname(), surveys);
     }
 }
